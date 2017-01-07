f06aa56a16405c9a1117ca9881bfc7619cc59fe2
Sergey Ukustov <sergey@ukstv.me>
2017-01-07 18:58:53 +0300

    Active link on top bar menu

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,11 @@
 $(document).foundation();
-$(() => {
-    $('pre code').each(function(i, block) {
-        hljs.highlightBlock(block);
+$(function () {
+    $("pre code").each(function(i, node) {
+        hljs.highlightBlock(node);
+    });
+
+    $("span.data-nav-li").each(function (i, node) {
+        var id = $(node).data("li-id");
+        $("#main-menu ul.menu.nav li#" + id).addClass("active");
     });
 });
