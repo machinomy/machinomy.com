1833826964a9c40e3b1305d3ca9bffa183bf8000
Sergey Ukustov <sergey@ukstv.me>
2017-03-26 23:57:02 +0300

    New tutorial "Inside a Payment Channel"

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,14 @@
 $(document).foundation();
-$(function () {
-    $("pre code").each(function(i, node) {
-        hljs.highlightBlock(node);
-    });
 
-    $("span.data-nav-li").each(function (i, node) {
-        var id = $(node).data("li-id");
-        $("#main-menu ul.menu.nav li#" + id).addClass("active");
-    });
+var conversionRates = {};
+
+$(function () {
+  $("pre code").each(function(i, node) {
+    hljs.highlightBlock(node);
+  });
+
+  $("span.data-nav-li").each(function (i, node) {
+    var id = $(node).data("li-id");
+    $("#main-menu ul.menu.nav li#" + id).addClass("active");
+  });
 });
